864fc4f424884a65fce80d867ec22bbcc8990547
Evy Tang <yvette_tang@outlook.com>
2020-07-16 18:57:18 +0100

    half way there

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,49 +1,37 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  // A special property in React component and is an object that includes
-  // any data that this component needs
-  // A property called 'state' which is then set to an object state
   state = {
     count: 0,
-    tags: [],
-    //["tag1", "tag2", "tag3"],
   };
-  /* One way to apply a style is to define a property here and
-   then reference it a JSX expression style={this.styles}
-  styles = {
-    fontSize: 50,
-    fontWeight: "bold",
+
+  // constructor() {
+  // super();
+  //{
+  /* "this" now is ALWAYS referencing the current counter object 1:06 */
+  //}
+  //this.handleIncrement = this.handleIncrement.bind(this);
+  //}
+
+  handleIncrement = (product) => {
+    console.log(product);
+    // console.log("Increment Clicked", this);
+    this.setState({ count: this.state.count + 1 });
   };
-  */
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}> {tag} </li>
-        ))}{" "}
-      </ul>
-    );
-  }
-
-  //Use {curly braces} to render something dynamically
-  // Within curly braces, add in any valic JavaScript expression
-  // An expression is something that produces a value
-
-  /*<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-  <button className="btn btn-secondary btn-sm">Increment</button> */
 
   render() {
     return (
       <div>
-        {this.state.tags.length === 0 && "Please create a new tag"}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={(product) => this.handleIncrement(product)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
-  /*
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
@@ -51,18 +39,9 @@ class Counter extends Component {
   }
 
   formatCount() {
-    //object destructuring (this.state.count), below, we structure the object
-    // by picking the count property. Here, we pick the count property of this
-    // object and are storing it in a seperate constant called count
     const { count } = this.state;
-    //If it the count is zero, we want to return the string 'Zero',
-    // otherwise we want to return the value of the count property itself
     return count === 0 ? "Zero" : count;
-
-    //can also return a JSX expression
-    // return count === 0 ? <h1>Zero</h1> : count;
   }
-  */
 }
 
 export default Counter;
