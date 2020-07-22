62b784371e883378f32df9feddb75f6b54906d30
Evy Tang <yvette_tang@outlook.com>
2020-07-22 14:06:44 +0100

    counter up to lifecycle hooks

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,30 +2,26 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-    ],
-  };
-
-  handleDelete = (counterId) => {
-    console.log("Event Handler Called", counterId);
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters });
-  };
-
   render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
     return (
       <div>
-        {this.state.counters.map((counter) => (
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
+          Reset
+        </button>
+        {counters.map((counter) => (
           <Counter
             key={counter.id}
-            onDelete={this.handleDelete}
-            value={counter.value}
-            id={counter.id}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+            //Instead of passing value, id and selected, could simply pass
+            // counter object itself as it contains everything we need to know
+            // about and if we needed to add a new property, counter carries
+            // all the data we need
+            counter={counter}
+            //value={counter.value}
+            //id={counter.id}
+            //selected={counter.selected}
           />
           /* Passing children prop<h4>Counter #{counter.id}</h4> */
         ))}
---
+++ src/components/localState.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    value: this.props.value,
+  };
+
+  // constructor() {
+  // super();
+  //{
+  /* "this" now is ALWAYS referencing the current counter object 1:06 */
+  //}
+  //this.handleIncrement = this.handleIncrement.bind(this);
+  //}
+
+  handleIncrement = (product) => {
+    // console.log("Increment Clicked", this);
+    this.setState({ value: this.state.value + 1 });
+  };
+
+  render() {
+    // Children prop console.log(this.props);
+
+    return (
+      <div>
+        {/* Pass children prop for complex thing such as
+          a dialogue box, you'd use:
+         {this.props.children}
+     */}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={(product) => this.handleIncrement(product)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
+  }
+}
+
+export default Counter;
